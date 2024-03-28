d7f4f0bff1dc095e10f2b6949dde927df7adbab2
Gustavo Vieira <gmvieirasousa@gmail.com>
2024-03-28 03:17:22 -0300

    Criação de cards

--- components/Home/AtividadeCard.tsx
+++ components/Home/AtividadeCard.tsx
@@ -4,18 +4,67 @@ import { LinearGradient } from "expo-linear-gradient";
 
 interface AtividadeProps {
   nomeMateria: string;
+  cor: string;
+  dia: string;
+  ext: string;
+  completa: string;
+  descricao: string;
 }
 
 const AtividadeCard: React.FC<AtividadeProps> = (props: AtividadeProps) => {
+  /*  function corParaRGBA(cor: string) {
+    const corMap = {
+      green: "0, 128, 0",
+      blue: "0, 0, 255",
+      red: "255, 0, 0",
+      // Adicione mais cores conforme necessário
+    };
+
+    // Retorna o valor RGBA correspondente ao nome da cor
+    return corMap[cor] || "0, 0, 0"; // Se a cor não estiver no mapa, retorna preto
+  }
+
+  // Função para escurecer a cor
+  function escurecerCor(cor: string, percentagem: number) {
+    const rgba = corParaRGBA(cor);
+    const [r, g, b] = rgba.split(", ");
+    const rEscurecido = Math.max(0, parseInt(r) * (1 - percentagem));
+    const gEscurecido = Math.max(0, parseInt(g) * (1 - percentagem));
+    const bEscurecido = Math.max(0, parseInt(b) * (1 - percentagem));
+
+    // Retorna a cor escurecida como uma string no formato RGBA
+    return `rgba(${rEscurecido}, ${gEscurecido}, ${bEscurecido}, 1)`;
+  }
+
+  const corEscurecida = escurecerCor(props.cor, 0.5);
+*/
   return (
-    <View className="bg-orange-600 w-40 h-52 m-1 rounded-xl">
-      <View className="items-center bg-slate-600 rounded-t-xl">
-        <Text className=" font-medium text-xl ">{props.nomeMateria}</Text>
+    <View className="bg-gray-300 border-green-300/40 border-2 w-40 h-52 m-1 rounded-lg">
+      <View className="items-center rounded-t-lg bg-black">
+        <Text className=" font-medium text-xl text-yellow-500 pb-1 pt-1 ">
+          {props.nomeMateria}
+        </Text>
+      </View>
+      <Text className=" font-extrabold text-3xl mt-5 p-2 text-center border-b-2 border-black/40">
+        {props.dia}
+      </Text>
+      <View className="flex-row">
+        <Text className=" text-xs w-16 text-center border-r-2 border-black/40 font-bold">
+          {props.ext}
+        </Text>
+        <Text className=" text-xs w-20 text-center pl-2 font-medium">
+          {props.completa}
+        </Text>
+      </View>
+      <View className=" mt-5 h-16 rounded-b-xl bg-black ">
+        <Text className=" text-gray-200 text-xs text-center p-2">
+          {props.descricao}
+        </Text>
       </View>
     </View>
   );
 };
-
+//         style={{ backgroundColor: `rgba(${corEscurecida},0.5)` }}
 const styles = StyleSheet.create({
   container: {},
 });
--- components/Home/HomeIndex.tsx
+++ components/Home/HomeIndex.tsx
@@ -9,11 +9,48 @@ const HomeIndex = () => {
     <View>
       <GabaritosCard />
       <View className=" mt-3">
-        <Text className=" text-3xl font-extrabold pl-2">Atividades:</Text>
+        <Text className=" text-4xl font-extrabold pl-2 pb-2">Atividades:</Text>
         <ScrollView horizontal={true} className=" ml-2">
-          <AtividadeCard nomeMateria="Biologia" />
-          <AtividadeCard nomeMateria="Matemática" />
-          <AtividadeCard nomeMateria="Física" />
+          <AtividadeCard
+            nomeMateria="Biologia"
+            cor="red"
+            dia="04"
+            ext="Quarta"
+            completa="04/06/2024"
+            descricao="Experimento para observar o processo de fotossíntese em plantas. Documente suas observações e discuta os principais conceitos envolvidos na produção de energia nas células vegetais."
+          />
+          <AtividadeCard
+            nomeMateria="Literatura"
+            cor="brown"
+            dia="17"
+            ext="Segundas"
+            completa="17/06/2024"
+            descricao="Resumo de um romance clássico da literatura brasileira e analise os principais temas, personagens e estilos literários presentes na obra. Escreva um ensaio crítico discutindo o impacto cultural e social do romance."
+          />
+          <AtividadeCard
+            nomeMateria="Matemática"
+            cor="orange"
+            dia="15"
+            ext="Segunda"
+            completa="15/06/2024"
+            descricao="Resolva uma série de problemas de trigonometria envolvendo os conceitos de seno, cosseno e tangente. Apresente suas soluções passo a passo e explique os princípios trigonométricos utilizados."
+          />
+          <AtividadeCard
+            nomeMateria="Física"
+            cor="green"
+            dia="19"
+            ext="Sexta"
+            completa="19/06/2024"
+            descricao="Investigar as leis do movimento de Newton. Utilize um carrinho e uma rampa inclinada para analisar a relação entre força, massa e aceleração."
+          />
+          <AtividadeCard
+            nomeMateria="Química"
+            cor="blue"
+            dia="23"
+            ext="Quinta"
+            completa="23/06/2024"
+            descricao="Escolher uma reação química e determine a ordem da reação, a constante de velocidade e os fatores que afetam a taxa de reação."
+          />
         </ScrollView>
       </View>
       <IndexCard />
